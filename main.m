b967b0bd31181bb8ce736fdaf04b68d96a6223d9
AlexandreVERNAY <91804493+AlexandreVERNAY@users.noreply.github.com>
2023-01-11 17:22:39 +0100

    Layout corrections

--- main.m
+++ main.m
@@ -1,119 +1,119 @@
-% Commandes de remise à zéro de Matlab
-close all; % Ferme toutes les fenêtres
+% Commandes d'initialisation de Matlab
+close all; % Ferme toutes les fenÃªtres
 clear;     % Efface toutes les variables
 clc;       % Efface le contenu de la console
 
-%% 1 - Chargment des valeurs des signaux
-Donnees_emission  = load('z_SignalRadarEMIS.txt'); % Chargments des valeurs du signal émis
-Donnees_reception = load('z_SignalEchoRadar2.txt');% Chargments des valeurs du signal reçu
+%% 1 - Chargement des valeurs des signaux
+Donnees_emission  = load('z_SignalRadarEMIS.txt'); % Signal Ã©mis
+Donnees_reception = load('z_SignalEchoRadar2.txt');% Signal reÃ§u
 
 %% 2 - Affectations des valeurs
-% Création des vecteurs de données
+% CrÃ©ation des vecteurs de donnÃ©es
 % Vecteur Temps
 Temps = Donnees_emission(:,1); % Toutes les valeurs de la colonne 1
 
 % Vecteur Signal radar emis
 Signal_emis = Donnees_emission(:, 2);
 
-% Vecteur Signal radar reçu 
+% Vecteur Signal radar reÃ§u 
 Signal_recu = Donnees_reception(:,2);
 
 % Affichage des signaux dans le domaine temporelle
-% Signal émis
+% Signal Ã©mis
 figure(11);
 plot(Temps, Signal_emis,'Color','#D95319')
-title('Signal émis dans le doamine temporel');
+title('Signal Ã©mis dans le doamine temporel');
 xlabel('Temps (s)');
 ylabel('Amplitude (V)');
 
-% Signal reçu
+% Signal reÃ§u
 figure(12);
 plot(Temps, Signal_recu,'Color','#7E2F8E')
-title('Signal reçu dans le doamine temporel');
+title('Signal reÃ§u dans le doamine temporel');
 xlabel('Temps (s)');
 ylabel('Amplitude (V)');
 
-% Signal émis et signal reçu
+% Signal Ã©mis et signal reÃ§u
 figure(13);
 hold on
-plot(Temps, Signal_emis,'Color','#D95319'); % Singal émis en temporelle
-plot(Temps, Signal_recu,'Color','#7E2F8E'); % Singal reçu en temporelle
+plot(Temps, Signal_emis,'Color','#D95319');
+plot(Temps, Signal_recu,'Color','#7E2F8E');
 title('Signaux dans le doamine temporel');
-legend('Signal emis', 'Signal reçu');
+legend('Signal emis', 'Signal reÃ§u');
 xlabel('Temps (s)');
 ylabel('Amplitude (V)');
 hold off
 
-%% 3 - Détermination des paramètres de la numérisation
+%% 3 - DÃ©termination des paramÃ¨tres de la numÃ©risation
 % Nombre de point du vecteur Temps
 NbPoint = length(Temps);
 
-% Fréquence d'échantillonnage
+% FrÃ©quence d'Ã©chantillonnage
 Fe = 1/(Temps(NbPoint)); 
 
-% Pas frequentiel
+% Pas frÃ©quentiel
 PasFreq = Fe/NbPoint;
 
-% Vecteur Fréquence Freq
+% Vecteur frÃ©quence
 Freq = PasFreq*((-(NbPoint-1)/2):((NbPoint-1)/2));
 
-% Temps echantillonage
-Te = Temps(2) - Temps(1); % Temps entre deux échantillon
+% Temps d'Ã©chantillonage
+Te = Temps(2) - Temps(1); % Temps entre deux Ã©chantillons
 
-%% 4 - Transformés de Fourier
-% Signal émis
-% TF du signal émis
+%% 4 - TransformÃ©es de Fourier
+% Signal Ã©mis
+% TF du signal Ã©mis
 TF_Signal_emis = fft(Signal_emis)/NbPoint;
 TF_Signal_emis = fftshift(TF_Signal_emis);
 
-% Module et Phase de la TF du signal Radar Emis
+% Module et Phase de la TF du signal radar Ã©mis
 Md_TF_Signal_emis  = abs(TF_Signal_emis);
 Ph_TF_Signal_emis  = 360*angle(TF_Signal_emis)/(2*pi);
 
-% Signal reçu
-% TF du signal reçu
+% Signal reÃ§u
+% TF du signal reÃ§u
 TF_Signal_recu = fft(Signal_recu)/NbPoint;
 TF_Signal_recu = fftshift(TF_Signal_recu);
 
-% Module et Phase de la TF du signal Signal_recu2
+% Module et Phase de la TF du signal radar reÃ§u
 Md_TF_Signal_recu  = abs(TF_Signal_recu);
 Ph_TF_Signal_recu  = 360*angle(TF_Signal_recu)/(2*pi);
 
-%Affichage des TF des signaux dans le domaine fréquentiel
+%Affichage des TF des signaux dans le domaine frÃ©quentiel
 
-% Module TF signal émis
+% Module TF signal Ã©mis
 figure(21);
 plot(Freq,Md_TF_Signal_emis,'Color','#D95319')
-title('Module de la TF du signal émis dans le domaine fréquentiel');
+title('Module de la TF du signal Ã©mis dans le domaine frÃ©quentiel');
 xlabel('Frequence (Hz)');
 ylabel('Amplitude (V)');
 
-% Phase TF signal émis
+% Phase TF signal Ã©mis
 figure(22);
 plot(Freq,Ph_TF_Signal_emis,'Color','#D95319')
-title('Phase de la TF du signal émis dans le domaine fréquentiel');
+title('Phase de la TF du signal Ã©mis dans le domaine frÃ©quentiel');
 xlabel('Frequence (Hz)');
-ylabel('Phase (Degré)');
+ylabel('Phase (DegrÃ©)');
 
-% Module TF signal reçu
+% Module TF signal reÃ§u
 figure(23);
 plot(Freq,Md_TF_Signal_recu,'Color','#7E2F8E')
-title('Module de la TF du signal reçu dans le domaine fréquentiel');
+title('Module de la TF du signal reÃ§u dans le domaine frÃ©quentiel');
 xlabel('Frequence (Hz)');
 ylabel('Amplitude (V)');
 
-% Phase TF signal reçu 
+% Phase TF signal reÃ§u 
 figure(24);
 plot(Freq,Ph_TF_Signal_recu,'Color','#7E2F8E')
-title('Phase de la TF du signal reçu dans le domaine fréquentiel');
+title('Phase de la TF du signal reÃ§u dans le domaine frÃ©quentiel');
 xlabel('Frequence (Hz)');
-ylabel('Phase (Degré)');
+ylabel('Phase (DegrÃ©)');
 
-%% 5 - Corrélation des signaux
-% Autocorrelation du signal émis
+%% 5 - CorrÃ©lation des signaux
+% AutocorrÃ©lation du signal Ã©mis
 AutoCorr_Signal_emis = xcorr(Signal_emis);
 
-% Intercorrelation du signal signal reçu et du signal émis
+% IntercorrÃ©lation du signal signal reÃ§u et du signal Ã©mis
 InterCorr_Signal_emis_Signal_recu = xcorr(Signal_recu,Signal_emis);
 
 % Affichage des signaux
@@ -121,7 +121,7 @@ figure(31);
 hold on
 plot(AutoCorr_Signal_emis,'Color','#D95319');
 plot(InterCorr_Signal_emis_Signal_recu,'Color','#7E2F8E');
-legend('autocorrélation émission ', 'intercorrelation reception avec émission ');
+legend('AutocorrÃ©lation Ã©mission ', 'Intercorrelation reception avec Ã©mission ');
 legend("Position",[0.49167,0.82817,0.53929,0.082143]);
 hold off
 
@@ -130,4 +130,4 @@ hold off
 
 vitesse_onde = 3e8;
 distance = (Decalage_temporel/2) * vitesse_onde;
-fprintf("Distance avion = %4.2f mètres", distance) %disp(distance)
+fprintf("Distance avion = %4.2f mÃ¨tres", distance) %disp(distance)
